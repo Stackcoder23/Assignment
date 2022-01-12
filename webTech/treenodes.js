84df39781572eea0cf5bdb3d2bb1e3165d92cf8f
Stackcoder <mjoshi23.mj@gmail.com>
2022-01-12 15:22:14 +0530

    commit

--- webTech/treenodes.js
+++ webTech/treenodes.js
@@ -1,7 +1,7 @@
-let nodes = [{ id: 0, parent: undefined }, { id: 1, parent: 0 }, { id: 2, parent: 1 }, { id: 4, parent: 2 }, { id: 3, parent: 2 }, { id: 6, parent: 3 }, { id: 5, parent: 4 }, { id: 7, parent: 6 }];
-console.table(nodes);
-let tobeRemovedIndex = 4;
-let toBeRemoved = [];
+// let nodes = [{ id: 0, parent: undefined }, { id: 1, parent: 0 }, { id: 2, parent: 1 }, { id: 4, parent: 2 }, { id: 3, parent: 2 }, { id: 6, parent: 3 }, { id: 5, parent: 4 }, { id: 7, parent: 6 }];
+// console.table(nodes);
+// let tobeRemovedIndex = 4;
+// let toBeRemoved = [];
 // var childrenNodes = function (tree, _nodes) {
 //     let children = [];
 //     (Array.isArray(_nodes) ? _nodes : [_nodes]).forEach(function (node) {
